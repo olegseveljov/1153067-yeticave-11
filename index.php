be76bf431d6ac54cd4feaae500cba47a1c379e80
olegseveljov <57150825+olegseveljov@users.noreply.github.com>
2019-10-30 16:55:50 +0100

    Hi

--- index.php
+++ index.php
@@ -26,27 +26,7 @@ $user_name = 'Oleg';
         </form>
         <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
-        <nav class="user-menu">
-
-            if ($is_auth) = rand(0, 1);
-
-         <div class="user-menu__logged">
-           <p>$user_name</p>
-           <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
-           <a class="user-menu__logout" href="#">Выход</a>
-         </div>
-
-
-            <ul class="user-menu__list">
-                <li class="user-menu__item <?php if == 0): ?>is_auth<?php endif ?>">
-                    <a href="#">Регистрация</a>
-                </li>
-                <li class="user-menu__item">
-                    <a href="#">Вход</a>
-                </li>
-            </ul>
-
-        </nav>
+        <nav class="user-menu"> </nav>
     </div>
 </header>
 
@@ -145,4 +125,3 @@ $user_name = 'Oleg';
 <script src="script.js"></script>
 </body>
 </html>
-<?php endif; ?>
