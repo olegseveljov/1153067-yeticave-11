e0bc487205e9cec0d0a08fc75e59a11a80e53281
olegseveljov <57150825+olegseveljov@users.noreply.github.com>
2019-10-30 17:23:04 +0100

    Hi

--- index.php
+++ index.php
@@ -30,12 +30,12 @@ $user_name = 'Oleg';
             <?php
             $is_auth = "0, 1";
 
-            if ($is_auth == 1)
+            if ($is_auth == 1) {
             <div class="user-menu__logged">
                 <p>$user_name</p>
                 <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
                 <a class="user-menu__logout" href="#">Выход</a>
-            </div>
+            </div> }
 
 </nav>
     </div>
