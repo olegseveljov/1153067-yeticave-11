695219156c1f8a5de20928b6133762c7d8f0583a
olegseveljov <57150825+olegseveljov@users.noreply.github.com>
2019-10-30 17:10:58 +0100

    Hi

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ $user_name = 'Oleg';
 
             if ($is_auth == 1) {
             <div class="user-menu__logged">
-                <p>#user_name#</p>
+                <p>$user_name</p>
                 <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
                 <a class="user-menu__logout" href="#">Выход</a>
             </div>
