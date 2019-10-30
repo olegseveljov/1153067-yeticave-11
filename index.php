b68798347485ec8a5e3233682fa4cea0b2e67a70
olegseveljov <57150825+olegseveljov@users.noreply.github.com>
2019-10-30 16:06:17 +0100

    Hi

--- index.php
+++ index.php
@@ -28,13 +28,15 @@ $user_name = 'Oleg';
 
         <nav class="user-menu">
 
-            <?php print $is_auth = 1;
+            $is_auth = 1;
 
             <div class="user-menu__logged">
                 <p>$user_name</p>
                 <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
                 <a class="user-menu__logout" href="#">Выход</a>
             </div>
+
+
             <ul class="user-menu__list">
                 <li class="user-menu__item">
                     <a href="#">Регистрация</a>
@@ -43,7 +45,6 @@ $user_name = 'Oleg';
                     <a href="#">Вход</a>
                 </li>
             </ul>
-            ?>
         </nav>
     </div>
 </header>
