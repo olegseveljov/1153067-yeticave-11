b9e93e3b0133b80829beeeb7d79bda233ad966fd
olegseveljov <57150825+olegseveljov@users.noreply.github.com>
2019-10-30 16:27:47 +0100

    Hi

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $is_auth = rand(0, 1);
 
-$user_name = ''; Oleg
+$user_name = 'Oleg';
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -27,14 +27,20 @@ $user_name = ''; Oleg
         <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
-            <?php if ($is_auth == "1"): ?>
+
+            <?php
+            $is_auth = rand(0, 1);
+            $user_name = 'Oleg';
+
+            if ($is_auth == rand 1):
 
             <div class="user-menu__logged">
                 <p>$user_name</p>
                 <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
                 <a class="user-menu__logout" href="#">Выход</a>
             </div>
-            <?php elseif ($is_auth == "1"): ?>
+
+            elseif ($is_auth == "1"):
 
             <ul class="user-menu__list">
                 <li class="user-menu__item">
@@ -44,6 +50,7 @@ $user_name = ''; Oleg
                     <a href="#">Вход</a>
                 </li>
             </ul>
+            ?>
         </nav>
     </div>
 </header>
