61187e65f4b372219f84f367ac887e1e2c56e67c
olegseveljov <57150825+olegseveljov@users.noreply.github.com>
2019-10-30 17:18:50 +0100

    Hi

--- index.php
+++ index.php
@@ -36,10 +36,8 @@ $user_name = 'Oleg';
                 <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
                 <a class="user-menu__logout" href="#">Выход</a>
             </div>
-            ?>>
 
-
-        </nav>
+/nav>
     </div>
 </header>
 
