29024e5e45855366ae0df0b13d37e1829daadf89
olegseveljov <57150825+olegseveljov@users.noreply.github.com>
2019-10-30 17:25:41 +0100

    Hi

--- index.php
+++ index.php
@@ -26,17 +26,7 @@ $user_name = 'Oleg';
         </form>
         <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
-        <nav class="user-menu">
-            <?php
-
-            if ($is_auth == 1) {
-            <div class="user-menu__logged">
-                <p>$user_name</p>
-                <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
-                <a class="user-menu__logout" href="#">Выход</a>
-            </div> }
-
-</nav>
+        <nav class="user-menu"</nav>
     </div>
 </header>
 
