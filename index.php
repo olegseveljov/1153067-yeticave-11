00598c80a748662d05125218336693ebeaed5ab6
olegseveljov <57150825+olegseveljov@users.noreply.github.com>
2019-11-12 18:38:27 +0100

    Feature: #4 - functions

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <?php
 
+require_once __DIR__ . '/helpers.php';
+
 $categories = [];
 
 $items = [];
 
 $page_content = include_template('inc/main.php' , ['$categories']);
 
-$layout_content = include_templat('layout.php', [
+$layout_content = include_template('layout.php', [
     'content' => $page_content,
     'categories' => $categories,
-    'Title' => 'YetiCave - Главная'
+    'title' => 'YetiCave - Главная'
 ]);
 
 print($layout_content);
