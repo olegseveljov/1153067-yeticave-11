caf7c163afa7d96f976439587212b3141dbb13ee
olegseveljov <57150825+olegseveljov@users.noreply.github.com>
2019-11-13 20:10:50 +0100

    Feature: #4 - functions

--- index.php
+++ index.php
@@ -1,23 +1,65 @@
 <?php
 
 require_once __DIR__ . '/helpers.php';
-function ($price){
-    return $price;
-}
 
-$layout_content = include_template('main.php' , [
-    'is_auth' => $is_auth,
+$is_auth = rand(0, 1);
+
+$user_name = 'Oleg';
+
+$categories = [
+    "Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"
+];
+
+$items = [
+    [
+        'image' => 'img/lot-1.jpg',
+        'title' => '2014 Rossignol District Snowboard',
+        'category' => 'Доски и лыжи',
+        'price' => 10999,
+    ],
+    [
+        'image' => 'img/lot-2.jpg',
+        'title' => 'DC Ply Mens 2016/2017 Snowboard',
+        'category' => 'Доски и лыжи',
+        'price' => 159999,
+    ],
+    [
+        'image' => 'img/lot-3.jpg',
+        'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+        'category' => 'Крепления',
+        'price' => 8000,
+    ],
+    [
+        'image' => 'img/lot-4.jpg',
+        'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
+        'category' => 'Ботинки',
+        'price' => 10999,
+    ],
+    [
+        'image' => 'img/lot-5.jpg',
+        'title' => 'Куртка для сноуборда DC Mutiny Charocal',
+        'category' => 'Одежда',
+        'price' => 7500,
+    ],
+    [
+        'image' => 'img/lot-6.jpg',
+        'title' => 'Маска Oakley Canopy',
+        'category' => 'Разное',
+        'price' => 5400,
+    ],
+];
+
+$content = include_template('main.php' , [
     'categories' => $categories,
-    'items' => $items
+    'items' => $items,
 ]);
 
-
-$content = include_template('main.php' , ['content' => 'title']);
-
-$layout_content = include_template('main/layout.php' , [
-    'content' => $page_content,
+$layout_content = include_template('layout.php' , [
     'categories' => $categories,
-    'title' => 'YetiCave - Главная'
+    'title' => 'YetiCave - Главная',
+    'user_name' => $user_name,
+    'is_auth' => $is_auth,
+    'content' => $content
 ]);
 
 print($layout_content);
--- templates/layout.php
+++ templates/layout.php
@@ -3,7 +3,7 @@
 <head>
 
     <meta charset="UTF-8">
-    <title><?= $title["Главная"]; ?></title>
+    <title><?= $title; ?></title>
     <link href="../css/normalize.min.css" rel="stylesheet">
     <link href="../css/style.css" rel="stylesheet">
 </head>
@@ -44,48 +44,8 @@
         </div>
     </header>
 
-    <main class="container"><?= $content; ?></main>
-        <section class="promo">
-            <h2 class="promo__title">Нужен стафф для катки?</h2>
-            <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
-            <ul class="promo__list">
-                <?php foreach ($categories as $category): ?>
-                    <li class="promo__item promo__item--boards">
-                        <a class="promo__link" href="pages/all-lots.html"><?= $category; ?></a>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
-        </section>
-        <section class="lots">
-            <div class="lots__header">
-                <h2>Открытые лоты</h2>
-            </div>
-            <ul class="lots__list">
-                <?php //var_dump($image); ?>
-
-                <?php foreach ($items as $category => $item): ?>
-
-                    <li class="lots__item lot">
-                        <div class="lot__image">
-                            <img src="<?= $item['image']; ?>" width="350" height="260" alt="<?= $item['title']; ?>">
-                        </div>
-                        <div class="lot__info">
-                            <span class="lot__category"><?= $item['category']; ?></span>
-                            <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $item['title']; ?></a></h3>
-                            <div class="lot__state">
-                                <div class="lot__rate">
-                                    <span class="lot__amount">Стартовая цена</span>
-                                    <span class="lot__cost"><?= priceFormat($item['price']); ?></span>
-                                </div>
-                                <div class="lot__timer timer">
-                                    12:23
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
-        </section>
+    <main class="container">
+        <?= $content; ?>
     </main>
 </div>
 
