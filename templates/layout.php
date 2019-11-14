e15cba5dffe768a71990895bbaef4b475d8f66b9
olegseveljov <57150825+olegseveljov@users.noreply.github.com>
2019-11-14 11:05:24 +0100

    Feature: #4 - functions

--- templates/layout.php
+++ templates/layout.php
@@ -54,7 +54,7 @@
         <ul class="nav__list container">
             <?php foreach ($categories as $category): ?>
                 <li class="nav__item">
-                    <a href="pages/all-lots.html"><?= $category; ?></a>
+                    <a href="pages/all-lots.html"><?= esc($category); ?></a>
                 </li>
             <?php endforeach; ?>
         </ul>
